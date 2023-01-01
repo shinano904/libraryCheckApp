6d278190cfadd2ed20c9d2393dab3ad70f34fd4c
shinano904 <tsukawatari@gmail.com>
2023-01-01 11:55:58 +0900

    index 画面レイアウト作成

--- librarySiteApp/views.py
+++ librarySiteApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 # HTTPResponseというクラスをインポート
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # View関数を任意に定義
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'librarySiteApp/index.html')
