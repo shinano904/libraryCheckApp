c309b4c339050ade4a21221070f1e60e87e70a81
shinano904 <tsukawatari@gmail.com>
2023-01-01 11:55:58 +0900

    index 画面レイアウト作成

--- librarySite/urls.py
+++ librarySite/urls.py
@@ -21,5 +21,5 @@ from librarySiteApp import views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('views/',views.index,name='index'), # views記載の関数のpathを追記
+    path('',views.index,name='index'), # views記載の関数のpathを追記
 ]
